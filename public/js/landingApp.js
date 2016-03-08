6765b20d6c726c15a3fc78ef4642b622b645128a
Marcelo Pasut <pasutmarcelo@gmail.com>
2016-03-07 21:25:30 -0300

    integro angular

---
+++ public/js/landingApp.js
@@ -0,0 +1,6 @@
+/**
+ * Created by boot on 3/7/16.
+ */
+angular.module('landingApp', [
+    'landingApp.controllers'
+]);
---
+++ public/js/landingControllers.js
@@ -0,0 +1,11 @@
+/**
+ * Created by boot on 3/7/16.
+ */
+'use strict';
+
+/* Controllers */
+
+angular.module('landingApp.controllers', []).
+    controller('registerController', function($scope) {
+        console.log('Register Controller');
+    });
