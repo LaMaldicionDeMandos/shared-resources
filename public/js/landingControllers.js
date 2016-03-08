d230bd0a68828aedb5b306143aa533775e18b915
Marcelo Pasut <pasutmarcelo@gmail.com>
2016-03-07 21:39:04 -0300

    implemento controller dummy de registro

--- public/js/landingControllers.js
+++ public/js/landingControllers.js
@@ -8,4 +8,9 @@
 angular.module('landingApp.controllers', []).
     controller('registerController', function($scope) {
         console.log('Register Controller');
+        $scope.user = {username:'', password: '', rePassword:'', email:''};
+
+        $scope.register = function() {
+            console.log(JSON.stringify($scope.user));
+        };
     });
