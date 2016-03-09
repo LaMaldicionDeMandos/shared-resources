a6b92dd93bea1c08ab2fe475b1762a0d516b6896
Marcelo Pasut <pasutmarcelo@gmail.com>
2016-03-08 23:34:20 -0300

    implemento unit test del front con carma, tube que instalar karma y jasmine

---
+++ test/front/prueba_test.js
@@ -0,0 +1,10 @@
+/**
+ * Created by boot on 3/8/16.
+ */
+describe('Karma', function() {
+    beforeEach(function() { });
+    afterEach(function() { });
+    it('should pass', function() {
+        expect(true).toBe(true);
+    });
+});
