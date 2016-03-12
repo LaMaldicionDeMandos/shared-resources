ddf342ce41438a532f8485a32703f36989688f2c
Marcelo Pasut <pasutmarcelo@gmail.com>
2016-03-11 23:50:25 -0300

    implemento el primer test de mocha

---
+++ test/back/prueba_mocha.js
@@ -0,0 +1,11 @@
+/**
+ * Created by boot on 3/11/16.
+ */
+var assert = require('assert');
+describe('Health', function() {
+    describe('some test', function() {
+        it('should pass', function() {
+            assert.ok(true);
+        });
+    });
+});
