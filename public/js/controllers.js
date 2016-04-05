abe7d773b374819e9b0bf2759f0afb6f60e5f912
Marcelo Pasut <pasutmarcelo@gmail.com>
2016-04-04 23:26:49 -0300

    Creo angular service y controllers mock para la app

---
+++ public/js/controllers.js
@@ -0,0 +1,9 @@
+/**
+ * Created by boot on 4/4/16.
+ */
+'use strict';
+
+/* Controllers */
+angular.module('app.controllers', []).
+    controller('mockController', function($scope) {
+    });
---
+++ public/js/services.js
@@ -0,0 +1,7 @@
+/**
+ * Created by boot on 4/4/16.
+ */
+angular.module('app.services', []).
+    factory('mockService', function($http, $q) {
+        return {};
+    });
