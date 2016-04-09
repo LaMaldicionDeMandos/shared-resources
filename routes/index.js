4050aca630b6cb4bbdf6e509ef05ad90dd51d3f4
Marcelo Pasut <pasutmarcelo@gmail.com>
2016-04-09 20:46:59 -0300

    Cambio el join us con el username del usuario

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ exports.index = function(req, res) {
   console.log("going to index");
   if (req.isAuthenticated()) {
     console.log("user authenticated, rendering main");
-    res.render('main', {activation: null});
+    res.render('main', {activation: null, user: req.user});
   } else {
     console.log("user not authenticated, rendering index");
     res.render('index');
@@ -11,5 +11,5 @@ exports.index = function(req, res) {
 };
 
 exports.main = function(req, res) {
-  res.render('main', {activation: null});
+  res.render('main', {activation: null, user: req.user});
 };
